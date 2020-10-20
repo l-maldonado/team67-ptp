819ce6e95d7c37c0bd0b154cb9e3791bce0551ba
ximena astrid <xabordac@gmail.com>
2020-10-19 22:51:30 -0500

    carga

--- lib/menu.py
+++ lib/menu.py
@@ -23,49 +23,15 @@ from app import app
 # Add the DS4A_Img
 ####################################################################################
 
-<<<<<<< HEAD
-#DS4A_Img = html.Div(
-#    children=[html.Img(src=app.get_asset_url("ds4a-img.svg"), id="ds4a-image",)],
-#)
-=======
 # DS4A_Img = html.Div(
 #    children=[html.Img(src=app.get_asset_url("ds4a-img.svg"), id="ds4a-image",)],
 # )
 DS4A_LOGO = "../static/images/correlation-one.png"
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
 
 #############################################################################
 # Menu Layout
 #############################################################################
 
-<<<<<<< HEAD
-def Navbar():
-    navbar = dbc.Navbar(
-        [
-            dbc.Col(dbc.NavbarBrand("Home", href="/home"), sm=3, md=2, className="mr-auto flex-nowrap mt-3 mt-md-0",),
-            dbc.NavbarToggler(id="navbar-toggler2"),
-            dbc.Col(
-                dbc.Nav(dbc.NavItem(dbc.NavLink("Recommender System", href="/recommender_system")), navbar=True),
-                width="auto"
-            ),
-            dbc.Col(
-                dbc.Nav(dbc.NavItem(dbc.NavLink("Clustering Analysis", href="/clustering_analysis")), navbar=True),
-                width="auto"
-            ),
-            dbc.Col(
-                dbc.Nav(dbc.NavItem(dbc.NavLink("Descriptive Analytics", href="/descriptive_analytics")), navbar=True),
-                width="auto"
-            ),
-            dbc.Col(
-                dbc.Nav(dbc.NavItem(dbc.NavLink("About Us", href="/about_us")), navbar=True),
-                width="auto"
-            ),
-        ],
-        color="#FEFEFE",
-    )
-
-    return navbar
-=======
 
 def Navbar():
     navbar = dbc.NavbarSimple(
@@ -85,4 +51,3 @@ def Navbar():
         color="#FEFEFE",
     )
     return navbar
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
