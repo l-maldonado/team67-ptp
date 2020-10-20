5c00eec4c2becb8701b415140d73434973fecae1
ximena astrid <xabordac@gmail.com>
2020-10-19 22:54:16 -0500

    carga

--- lib/clustering_analysis.py
+++ lib/clustering_analysis.py
@@ -6,44 +6,16 @@ from dash.dependencies import Input, Output, State, ClientsideFunction
 from dash.exceptions import PreventUpdate
 import dash_core_components as dcc
 import dash_html_components as html
-<<<<<<< HEAD
-import plotly.express as px
-import pandas as pd 
-=======
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
 
 # Dash Bootstrap Components
 import dash_bootstrap_components as dbc
 
 # PLACE THE COMPONENTS IN THE LAYOUT
 
-<<<<<<< HEAD
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-
-=======
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
 layout = html.Div(
     [
         dbc.Alert(
             [
-<<<<<<< HEAD
-                html.H1("Clustering Analysis",style={'color':'#F37126'}),
-                html.P("Place to pay analytical platform for the analysis of the transactional behavior of your customers",style={'color':'#8190A5','font-weight': 'bold'}),
-            ],
-            style={'background-color':'#F8F6F6','border':'0px'},
-        ),
-        dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-=======
                 html.H3("Clustering Analysis", style={"color": "#F37126"}),
                 html.P(
                     "You will find an analysis of how your clients relate to"
@@ -53,6 +25,5 @@ layout = html.Div(
             ],
             style={"background-color": "#F8F6F6", "border": "0px"},
         ),
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
     ]
 )
--- lib/descriptive_analytics.py
+++ lib/descriptive_analytics.py
@@ -7,44 +7,16 @@ from dash.exceptions import PreventUpdate
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-<<<<<<< HEAD
-import pandas as pd 
-import seaborn as sns
-
-=======
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
 
 # Dash Bootstrap Components
 import dash_bootstrap_components as dbc
 
 # PLACE THE COMPONENTS IN THE LAYOUT
-<<<<<<< HEAD
-user_cols = ['transaction_processing_amount','isic_section_name']
-bd=pd.read_csv('data/bdfn.csv', usecols=user_cols)
-bd['transaction_processing_amount'] = bd['transaction_processing_amount'].str.replace(',','.').astype('float64')
-bd_bounded_10000 = bd[(bd["transaction_processing_amount"]>=1000) & (bd["transaction_processing_amount"]<10000)]
-
-fig = px.box(bd_bounded_10000, x="isic_section_name", y="transaction_processing_amount")
-=======
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
 
 layout = html.Div(
     [
         dbc.Alert(
             [
-<<<<<<< HEAD
-                html.H1("Descriptive Analytics",style={'color':'#F37126'}),
-                html.P("Place to pay analytical platform for the analysis of the transactional behavior of your customers",style={'color':'#8190A5','font-weight': 'bold'}),
-            ],
-            style={'background-color':'#F8F6F6','border':'0px'},
-        ),
-         dcc.Graph(
-        id='boxplot-graph',
-        figure=fig
-         )
-    ]
-)
-=======
                 html.H3("Descriptive Analytics", style={"color": "#F37126"}),
                 html.P(
                     "You will be able to found some important insights about "
@@ -104,4 +76,3 @@ grap = dbc.Container(
     ],
     fluid=True,
 )
->>>>>>> 9841020fcdef8153e13c908d099d065074d7defe
