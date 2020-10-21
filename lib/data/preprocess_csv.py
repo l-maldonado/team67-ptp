f203c7ef53e66a2b004c21396abc6ad42ca01706
edward0rtiz <931@holbertonschool.com>
2020-10-20 21:19:41 -0500

    [feature] create a df depending the csv to be loaded

---
+++ lib/data/preprocess_csv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+preprocess_var = __import__("preprocessor").preprocess_var
+preprocess_mul = __import__("preprocessor").preprocess_mul
+
+# Getting
+filename = "e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/placetopayDB4.ftr"
+preprocess_var(filename, "transaction_card_installments")
+
+
+# def preprocess_mul(bd, var1, var2):
