163deb8357972ad1478523a8f744aa840906e104
edward0rtiz <931@holbertonschool.com>
2020-10-21 22:57:19 -0500

    [update] update preprocessor

--- lib/data/preprocess_csv.py
+++ lib/data/preprocess_csv.py
@@ -5,7 +5,8 @@ preprocess_mul = __import__("preprocessor").preprocess_mul
 
 # Getting
 filename = "e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/placetopayDB4.ftr"
-preprocess_var(filename, "transaction_card_installments")
+# preprocess_var(filename, "transaction_card_installments")
 
 
 # def preprocess_mul(bd, var1, var2):
+preprocess_mul(filename, "isic_section_name", "transaction_processing_amount")
