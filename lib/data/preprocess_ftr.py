c40cc6ea3804e15e20a64281e0d711457525a1e8
edward0rtiz <931@holbertonschool.com>
2020-10-24 14:53:16 -0500

    [update] add single var fucntion

--- lib/data/preprocess_ftr.py
+++ lib/data/preprocess_ftr.py
@@ -2,7 +2,12 @@ import pandas as pd
 import feather
 
 preprocess_mf = __import__("preprocessor").preprocess_mf
+preprocess_sf = __import__("preprocessor").preprocess_sf
 
 filename = "e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/placetopayDB4.ftr"
 
 preprocess_mf(filename, "isic_section_name", "transaction_processing_amount")
+
+
+# TRANSACTION DESCRIPTION
+# preprocess_sf(filename, "transaction_description")
