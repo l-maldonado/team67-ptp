308d0ac96903e251b3b638c5decd06403f446ddf
edward0rtiz <931@holbertonschool.com>
2020-10-24 15:03:10 -0500

    [update] change var

--- index.py
+++ index.py
@@ -59,7 +59,7 @@ app.layout = html.Div(
 
 # Descriptive analytics
 descriptive_layout = html.Div(
-    [descriptive_analytics.layout, descriptive_analytics.boxplot_1]
+    [descriptive_analytics.layout, descriptive_analytics.transaction_amount]
 )
 
 # Clustering Anaysis
