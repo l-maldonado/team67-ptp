845cb66e9320f70950d191a4c56e32d461d30d26
edward0rtiz <931@holbertonschool.com>
2020-10-24 14:53:44 -0500

    [update] replace path into vars

--- lib/data/preprocessor.py
+++ lib/data/preprocessor.py
@@ -10,11 +10,13 @@ import os
 import feather
 
 # filepath = "e:/Users/edwar/Desktop/place_to_pay/team67-ptp/data/placetopayDB4.ftr"  # or 'my_data.feather'
-
 # data = feather.read_dataframe(filepath)
 
 ###### PREPROCESOR SINGLE VARIABLE ###########
 def preprocess_var(bd, var):
+    filepath_sv = (
+        f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/db/{var}.csv"
+    )
     filepath = bd
     data = feather.read_dataframe(filepath)
     df = data.copy()
@@ -22,11 +24,11 @@ def preprocess_var(bd, var):
     df2 = df2.to_frame()
     if df2[var].dtype is "category":
         df2[var] = df2[var].astype("category").cat.codes
-        filename = f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var}.csv"
+        filename = filepath_sv
         df2.to_csv(filename, index=False)
         print("Succesfully exported to csv")
     else:
-        filename = f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var}.csv"
+        filename = filepath_sv
         df2.to_csv(filename, index=False)
         print("Succesfully exported to csv")
 
@@ -35,6 +37,9 @@ def preprocess_var(bd, var):
 
 ###### PREPROCESOR MULTIPLE VARIABLE ###########
 def preprocess_mul(bd, var1, var2):
+    filepath_mv = (
+        f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/db/{var1}_{var2}.csv"
+    )
     filepath = bd
     data = feather.read_dataframe(filepath)
     df = data.copy()
@@ -43,37 +48,51 @@ def preprocess_mul(bd, var1, var2):
     if df2[var1].dtype is "category" and df2[var2].dtype is "category":
         df2[var1] = df2[var1].astype("category").cat.codes
         df2[var2] = df2[var2].astype("category").cat.codes
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.csv"
-        )
+        filename = filepath_mv
         df2.to_csv(filename, index=False)
         print("Succesfully exported to csv")
     elif df2[var1].dtype is "category" and df2[var2].dtype is not "category":
         df2[var1] = df2[var1].astype("category").cat.codes
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.csv"
-        )
+        filename = filepath_mv
         df2.to_csv(filename, index=False)
         print("Succesfully exported to csv")
     elif df2[var1].dtype is not "category" and df2[var2].dtype is "category":
         df2[var2] = df2[var2].astype("category").cat.codes
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.csv"
-        )
+        filename = filepath_mv
         df2.to_csv(filename, index=False)
         print("Succesfully exported to csv")
     else:
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.csv"
-        )
+        filename = filepath_mv
         df2.to_csv(filename, index=False)
         print("Succesfully exported to csv")
 
 
-# Example: preprocess_mul(data, "transaction_card_installments", "transaction_payer_id")
+######### PREPROCESSOR FEATHER SINGLE VAR ######################
+def preprocess_sf(bd, var):
+    filepath_svf = (
+        f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/db/{var}.ftr"
+    )
+    filepath = bd
+    data = feather.read_dataframe(filepath)
+    df = data.copy()
+    df2 = df[var]
+    df2 = df2.to_frame()
+    if df2[var].dtype is "category":
+        df2[var] = df2[var].astype("category").cat.codes
+        filename = filepath_svf
+        df2.to_feather(filename)
+        print("Succesfully exported to feather")
+    else:
+        filename = filepath_svf
+        df2.to_feather(filename)
+        print("Succesfully exported to feather")
+
 
 ###### PREPROCESOR MULTIPLE VARIABLE ###########
 def preprocess_mf(bd, var1, var2):
+    filepath_mvf = (
+        f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/db/{var1}_{var2}.ftr"
+    )
     filepath = bd
     data = feather.read_dataframe(filepath)
     df = data.copy()
@@ -82,28 +101,20 @@ def preprocess_mf(bd, var1, var2):
     if df2[var1].dtype is "category" and df2[var2].dtype is "category":
         df2[var1] = df2[var1].astype("category").cat.codes
         df2[var2] = df2[var2].astype("category").cat.codes
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.ftr"
-        )
+        filename = filepath_mvf
         df2.to_csv(filename)
         print("Succesfully exported to feather")
     elif df2[var1].dtype is "category" and df2[var2].dtype is not "category":
         df2[var1] = df2[var1].astype("category").cat.codes
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.ftr"
-        )
+        filename = filepath_mvf
         df2.to_csv(filename)
         print("Succesfully exported to feather")
     elif df2[var1].dtype is not "category" and df2[var2].dtype is "category":
         df2[var2] = df2[var2].astype("category").cat.codes
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.ftr"
-        )
-        df2.to_csv(filename)
+        filename = filepath_mvf
+        df2.to_feather(filename)
         print("Succesfully exported to feather")
     else:
-        filename = (
-            f"e:/Users/edwar/Desktop/place_to_pay/team67-ptp/lib/data/{var1}_{var2}.ftr"
-        )
+        filename = filepath_mvf
         df2.to_feather(filename)
         print("Succesfully exported to feather")
