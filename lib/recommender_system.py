daec32a44173b10529fb48c3055cc96db7bc9ca6
edward0rtiz <931@holbertonschool.com>
2020-11-07 08:27:13 -0500

    [update] WIP inser recommender system

--- lib/recommender_system.py
+++ lib/recommender_system.py
@@ -7,6 +7,7 @@ from dash.exceptions import PreventUpdate
 import dash_core_components as dcc
 import dash_html_components as html
 
+app = __import__("app").app
 # Dash Bootstrap Components
 import dash_bootstrap_components as dbc
 
@@ -18,7 +19,7 @@ layout = html.Div(
             [
                 html.H3("Recommender System", style={"color": "#F37126"}),
                 html.P(
-                    "Recommendation system that gives you the list of products that each of yur clienes are model likely to buy",
+                    "Recommendation system that gives you the list of products that each of your clients are model likely to buy",
                     style={"color": "#8190A5", "font-weight": "bold"},
                 ),
             ],
@@ -26,3 +27,42 @@ layout = html.Div(
         ),
     ]
 )
+
+
+input_form = dbc.Row(
+    dbc.Col(
+        html.Div(
+            [
+                html.P("Type merchant Id"),
+                dbc.InputGroup(
+                    [
+                        dbc.InputGroupAddon("CO000000", addon_type="prepend"),
+                        dbc.Input(
+                            id="input",
+                            placeholder="Enter last 4 digits of merchant_id",
+                            type="text",
+                            size="20",
+                        ),
+                        html.Br(),
+                        html.P(id="output"),
+                        dbc.Button(
+                            "Submit",
+                            outline=True,
+                            color="link",
+                            style={"color": "#F37126"},
+                        ),
+                    ],
+                    className="sm-3",
+                ),
+            ],
+        ),
+        width={"size": 6, "offset": 3},
+    )
+)
+
+html.Datalist(id="browser", children=[html.Option(value="CO000000")]),
+
+
+@app.callback(Output("output", "children"), [Input("input", "value")])
+def output_text(value):
+    return None
