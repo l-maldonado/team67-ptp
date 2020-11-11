348068101330413194325a0cf6bd56c96c3c9201
ximena astrid <xabordac@gmail.com>
2020-11-10 21:05:20 -0500

    actualizado index

--- index.py
+++ index.py
@@ -115,7 +115,7 @@ descriptive_layout = html.Div(
 clustering_layout = html.Div([clustering_analysis.layout])
 
 # Recommender System
-recommender_layout = html.Div([recommender_system.layout])
+recommender_layout = html.Div([recommender_system.layout, recommender_system.form])
 
 # About us
 about_layout = html.Div([about_us.layout])
