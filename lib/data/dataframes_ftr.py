b83c172c46027bd80f62e3a10331d324c9294ce0
edward0rtiz <931@holbertonschool.com>
2020-11-14 19:52:01 -0500

    [update] add new db for payerid recosys

--- lib/data/dataframes_ftr.py
+++ lib/data/dataframes_ftr.py
@@ -2,6 +2,18 @@ import pandas as pd
 import numpy as np
 import feather
 
-filepath1 = "data/similarities.feather"
-ds_x = feather.read_dataframe(filepath1)
-ds_x
+
+filename1 = "data/bdsita.feather"
+df_x = feather.read_dataframe(filename1)
+df_x.rename(
+    columns={"transaction_payer_id": "user",
+             "merchant_id": "item"}, inplace=True
+)
+df_x
+
+filename2 = "data/cluster2.feather"
+df_c = feather.read_dataframe(filename2)
+df_c
+
+cl_0 = df_c[df_c['cluster_predicted'] == 0]
+cl_1 = df_c[df_c['cluster_predicted'] == 1]
