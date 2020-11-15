4bcb54ef13178176291243ec5d405c066ac6a1f2
edward0rtiz <931@holbertonschool.com>
2020-11-15 15:58:45 -0500

    [fix] call df from cache

--- lib/recommender_system.py
+++ lib/recommender_system.py
@@ -9,9 +9,12 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from app import app
 from .data.dataframes import ds_x
-from .data.dataframes_ftr import df_x
+#from .data.dataframes_ftr import df_x
 app = __import__("app").app
+#get_db = __import__("data.dataframes_ftr").get_db
+from .data.dataframes_ftr import get_db
 
+df_x = get_db()
 
 # PLACE THE COMPONENTS IN THE LAYOUT
 layout = html.Div(
