2f63c2de6f1c790b1c3e34ef3265e5f46ad61d8c
edward0rtiz <931@holbertonschool.com>
2020-11-15 14:59:10 -0500

    [update] add def_cf to use momoization in app to reduce time in loading a df

--- lib/data/dataframes_ftr.py
+++ lib/data/dataframes_ftr.py
@@ -4,6 +4,8 @@ File to import csv file in data dir and convert into dataframe
 import pandas as pd
 import numpy as np
 import feather
+from app import cache
+
 
 # Import payer_id and merchant_id for recommendation system
 filename1 = "data/bdsita.feather"
@@ -14,10 +16,12 @@ df_x.rename(
 )
 df_x
 
-# Export cluster file for recommendation system
-filename2 = "data/cluster2.feather"
-df_c = feather.read_dataframe(filename2)
-df_c
 
-cl_0 = df_c[df_c['cluster_predicted'] == 0]
-cl_1 = df_c[df_c['cluster_predicted'] == 1]
+filename2 = "data/cluster2.feather"
+@cache.memoize(timeout=120)
+def get_df(filename=filename2):
+    # Export cluster file for recommendation system
+    df_c = feather.read_dataframe(filename)
+    return df_c
+
+get_df()
